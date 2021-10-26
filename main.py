9dfa1e87676d007e835ec93f101edfe5cc0509fa
beasticle <julian.heidt@gmail.com>
2021-10-25 23:19:44 -0400

    isn't getting the domain names correctly

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ import pandas as pd
 workbook = pd.read_excel("EMEATeamsPermissions.xlsx", sheet_name="EMEATeamsPermissions")
 workbook.head()
 
-#count = 2
-
-#teamName = workbook['Teamname'].iloc(count)
-
+count = 0
 memberList = []
+#domainNames = workbook['Member Mail']
 
-for teamName in workbook['Teams Name']:
-    count = 2
-    count += 1
-    print(teamName)
-    if teamName == workbook['Teams Name'].iloc[count]:
-        prevTeamName = workbook['Teams Name'].iloc[count]
-        memberList.append(workbook['Member Name'].iloc[count])
+for index, domainName in enumerate(workbook['Member Mail'], start=1):
+    print(domainName + f" {count}")
+    # count += 1
+    if domainName == workbook['Member Mail'].iloc[index]:
+        print(domainName + " " + workbook['Member Mail'].iloc[index].split('@')[1] + f"\n")
+        #currentDN = workbook['Member Mail'].iloc[count]
+        #print(memberDict)
+        memberList.append(workbook['Member Name'].iloc[index] + " " + workbook['Member Mail'].iloc[index].split('@')[1])
         df = pd.DataFrame(memberList)
-        df.to_excel("EMEATeamsPermissions.xlsx", sheet_name="Memberoutput")
+        df.to_excel("Test.xlsx", sheet_name="Memberoutput")
+        
+    # else:
+    #     print("how the fuck did we get here?")
 
