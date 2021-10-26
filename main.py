79644105b6930a7a89f27597a29d5067f82e3e24
Julian Heidt <julian.heidt@gmail.com>
2021-10-26 10:57:14 -0400

    this code parse's through the excel spreadsheet and returns users by domain name

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
+from collections import defaultdict
 import pandas as pd
 
 workbook = pd.read_excel("EMEATeamsPermissions.xlsx", sheet_name="EMEATeamsPermissions")
 workbook.head()
 
-count = 0
-memberList = []
+memberDict = defaultdict(list)
 #domainNames = workbook['Member Mail']
 
-for index, domainName in enumerate(workbook['Member Mail'], start=1):
-    print(domainName + f" {count}")
+for index, domainName in enumerate(workbook['Member Mail']):
+    print(domainName + f" {index}")
     # count += 1
     if domainName == workbook['Member Mail'].iloc[index]:
         print(domainName + " " + workbook['Member Mail'].iloc[index].split('@')[1] + f"\n")
         #currentDN = workbook['Member Mail'].iloc[count]
         #print(memberDict)
-        memberList.append(workbook['Member Name'].iloc[index] + " " + workbook['Member Mail'].iloc[index].split('@')[1])
-        df = pd.DataFrame(memberList)
-        df.to_excel("Test.xlsx", sheet_name="Memberoutput")
+        # memberDict.append(workbook['Member Name'].iloc[index] + " " + workbook['Member Mail'].iloc[index].split('@')[1])
+        memberDict[workbook['Member Mail'].iloc[index].split('@')[1]].append(workbook['Member Name'].iloc[index])
+        if index == 7229:
+            df = pd.DataFrame.from_dict([memberDict])
+            df.to_excel("Test.xlsx", sheet_name="Memberoutput")
+            print("member list: " + f'{memberDict}')
+            # break
         
     # else:
     #     print("how the fuck did we get here?")
