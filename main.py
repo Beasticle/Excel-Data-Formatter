d369da032a4e212d8b05ad39e914d04c394aa925
Julian Heidt <julian.heidt@gmail.com>
2021-10-26 14:30:59 -0400

    formatted output

--- main.py
+++ main.py
@@ -14,14 +14,19 @@ for index, domainName in enumerate(workbook['Member Mail']):
         print(domainName + " " + workbook['Member Mail'].iloc[index].split('@')[1] + f"\n")
         #currentDN = workbook['Member Mail'].iloc[count]
         #print(memberDict)
-        # memberDict.append(workbook['Member Name'].iloc[index] + " " + workbook['Member Mail'].iloc[index].split('@')[1])
-        memberDict[workbook['Member Mail'].iloc[index].split('@')[1]].append(workbook['Member Name'].iloc[index])
+        if workbook['Member Mail'].iloc[index].split('@')[1] not in memberDict:
+            memberDict[workbook['Member Mail'].iloc[index].split('@')[1]] = [workbook['Member Name'].iloc[index]]
+            if workbook['Member Name'].iloc[index] not in memberDict[workbook['Member Mail'].iloc[index].split('@')[1]]:
+                memberDict[workbook['Member Mail'].iloc[index].split('@')[1]].append(workbook['Member Name'].iloc[index])
+            else:
+                print("Value is already in that list")
+        else:
+            if workbook['Member Name'].iloc[index] not in memberDict[workbook['Member Mail'].iloc[index].split('@')[1]]:
+                memberDict[workbook['Member Mail'].iloc[index].split('@')[1]].append(workbook['Member Name'].iloc[index])
+            else:
+                print("Value is already in that list")
+            # memberDict[workbook['Member Mail'].iloc[index].split('@')[1]].append(workbook['Member Name'].iloc[index])
         if index == 7229:
-            df = pd.DataFrame.from_dict([memberDict])
+            df = pd.DataFrame.from_dict(memberDict, orient='index')
             df.to_excel("Test.xlsx", sheet_name="Memberoutput")
             print("member list: " + f'{memberDict}')
-            # break
-        
-    # else:
-    #     print("how the fuck did we get here?")
-
