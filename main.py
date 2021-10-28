cb1db313432590b8f0b762d6c27a5b25374ba98b
Julian Heidt <julian.heidt@gmail.com>
2021-10-28 16:44:57 -0400

    commented where error is

--- main.py
+++ main.py
@@ -1,35 +1,46 @@
 from collections import defaultdict
 import pandas as pd
 
-print("Please put your excel workbook in the same folder as this script. All spelling needs to be exact.")
+print("\n Please put your excel workbook in the same folder as this script. All spelling needs to be exact.")
 
-sheetName = input("What is the name of your sheet?")
+workbookName = input("\n What is the name of your workbook? ")
 
-indexCol = input('What column is going to be your index?')
+sheetName = input("\n What is the name of your sheet? ")
 
-columns = input("What columns is your data in? If you have multiple columns, please format them as 'Col1, Col2, etc'")
+indexCol = input("\n What column is going to be your index? ")
 
-workbook = pd.read_excel("*.xlsx", sheet_name=sheetName)
+columns = input("\n What columns is your data in? If you have multiple columns, please format them as 'Col1, Col2, etc' ")
+
+filterInput = input("\n What do you want to filter by, eg. scm.uk? ")
+
+workbook = pd.read_excel(f"{workbookName}.xlsx", sheet_name=sheetName)
 workbook.head()
 
 indexDict = defaultdict(list)
-dataColList = columns.split(", ")
+colList = columns.split(", ")
 
-for index, data in enumerate(workbook['Member Mail']):
+for data in colList:
+    columnList = []
+    # problem is here, idfk
+    columnList.append(colList[data].strip(', '))
 
-    if data == workbook['Member Mail'].iloc[index]:
+filterList = filterInput.split(", ")
 
-        print(data + " " + workbook['Member Mail'].iloc[index].split('@')[1] + f"\n")
+for index, data in enumerate(workbook[indexCol]):
 
-        if workbook['Member Mail'].iloc[index].split('@')[1] not in indexDict and workbook['Member Mail'].iloc[index].split('@')[1] in dataColList: # or 'smc.uk':
-            indexDict[workbook[indexCol].iloc[index]] = [workbook['Member Mail'].iloc[index].split('@')[1], workbook['Teams Name'].iloc[index], workbook['Role'].iloc[index]]
+    if data == workbook[indexCol].iloc[index]:
+        print(columnList)    
+        print(data, index)
 
-        elif workbook['Member Mail'].iloc[index].split('@')[1] in dataColList: # or 'smc.uk':
-            indexDict[workbook[indexCol].iloc[index]].extend([workbook['Member Mail'].iloc[index].split('@')[1], workbook['Teams Name'].iloc[index], workbook['Role'].iloc[index]])
+        if workbook[columnList[0]].iloc[index].split('@')[1] not in indexDict and workbook[columnList[0]].iloc[index].split('@')[1] in filterList:
+            indexDict[workbook[indexCol].iloc[index]] = [workbook[columnList[0]].iloc[index].split('@')[1], workbook[columnList[1]].iloc[index], workbook[columnList[2]].iloc[index]]
+
+        elif workbook[colList[0]].iloc[index].split('@')[1] in filterList:
+            indexDict[workbook[indexCol].iloc[index]].extend([workbook[columnList[0]].iloc[index].split('@')[1], workbook[columnList[1]].iloc[index], workbook[columnList[2]].iloc[index]])
         else:
             print("Value is already in that list or not valid")
 
         if index == 7229:
             df = pd.DataFrame.from_dict(indexDict , orient='index')
-            df.to_excel("/Output/Test.xlsx", sheet_name="Memberoutput")
+            df.to_excel("Test.xlsx", sheet_name="Memberoutput")
             print("member list: " + f'{indexDict }')
