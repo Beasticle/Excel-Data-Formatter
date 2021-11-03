1335affd6bf255e71e57c2e5360cec6b739e23ea
Julian Heidt <julian.heidt@gmail.com>
2021-11-03 14:56:47 -0400

    starting to make it more versitile

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from numpy import radians
 import pandas as pd
 
 print("\n Please put your excel workbook in the same folder as this script. All spelling needs to be exact.")
@@ -20,8 +21,8 @@ indexDict = defaultdict(list)
 colList = columns.split(", ")
 filterList = filterInput.split(', ')
 stringToParse = ""
-string1 = ""
-string2 = ""
+# string1 = ""
+# string2 = ""
 columnList = []
 filterListFull = []
 for data in range(len(filterList)):
@@ -30,30 +31,62 @@ for data in range(len(colList)):
     columnList.append(colList[data].strip(', '))
 
 for data in columnList:
-    if "@" in workbook[data].iloc[0]:
-        stringToParse = data
-    elif string1 == "":
-        string1 = data
-    elif string1 != "":
-        string2 = data
+    if "@" not in workbook[data].iloc[0]:
+        for i in range(len(workbook[data])):
+            globals()[f"string{i}"] = data
+            noformat = True
+    else: 
+        if "@" in workbook[data].iloc[0]:
+            stringToParse = data
+            noformat = False
+        else:
+            for i in enumerate(workbook[data], start=1):
+                globals()[f"string{i}"] = data
 
 filterList = filterInput.split(", ")
 
-for index, data in enumerate(workbook[indexCol]):
+def formatFunction():
 
-    if data == workbook[indexCol].iloc[index]:
-        print(columnList)    
-        print(data, index)
+    for index, data in enumerate(workbook[indexCol]):
 
-        if workbook[stringToParse].iloc[index].split('@')[1] not in indexDict and workbook[stringToParse].iloc[index].split('@')[1] in filterListFull:
-            indexDict[workbook[indexCol].iloc[index]] = [workbook[stringToParse].iloc[index].split('@')[1], workbook[string1].iloc[index], workbook[string2].iloc[index]]
+        if data == workbook[indexCol].iloc[index]:
+            print(columnList)    
+            print(data, index)
 
-        elif workbook[stringToParse].iloc[index].split('@')[1] in filterListFull:
-            indexDict[workbook[indexCol].iloc[index]].extend([workbook[stringToParse].iloc[index].split('@')[1], workbook[string1].iloc[index], workbook[string2].iloc[index]])
-        else:
-            print("Value is already in that list or not valid")
+            if workbook[stringToParse].iloc[index].split('@')[1] not in indexDict and workbook[stringToParse].iloc[index].split('@')[1] in filterListFull:
+                indexDict[workbook[indexCol].iloc[index]] = [workbook[stringToParse].iloc[index].split('@')[1], workbook[string1].iloc[index], workbook[string2].iloc[index]]
 
-        if index == 7229:
-            df = pd.DataFrame.from_dict(indexDict , orient='index')
-            df.to_excel("Test.xlsx", sheet_name="Memberoutput")
-            print("member list: " + f'{indexDict }')
+            elif workbook[stringToParse].iloc[index].split('@')[1] in filterListFull:
+                indexDict[workbook[indexCol].iloc[index]].extend([workbook[stringToParse].iloc[index].split('@')[1], workbook[string1].iloc[index], workbook[string2].iloc[index]])
+            else:
+                print("Value is already in that list or not valid")
+
+            if index == 7229:
+                df = pd.DataFrame.from_dict(indexDict , orient='index')
+                df.to_excel("Test.xlsx", sheet_name="Memberoutput")
+                print("member list: " + f'{indexDict }')
+
+def noFormattingFunction():
+    for index, data in enumerate(workbook[indexCol]):
+
+        if data == workbook[indexCol].iloc[index]:
+            print(columnList)    
+            print(data, index)
+
+            if workbook[string0].iloc[index] not in indexDict and workbook[string0].iloc[index] in filterListFull:
+                indexDict[workbook[indexCol].iloc[index]] = [workbook[string0].iloc[index], workbook[string1].iloc[index], workbook[string2].iloc[index]]
+
+            elif workbook[string0].iloc[index] in filterListFull:
+                indexDict[workbook[indexCol].iloc[index]].extend([workbook[string0].iloc[index], workbook[string1].iloc[index], workbook[string2].iloc[index]])
+            else:
+                print("Value is already in that list or not valid")
+
+            if index == 7229:
+                df = pd.DataFrame.from_dict(indexDict , orient='index')
+                df.to_excel("Test.xlsx", sheet_name="Memberoutput")
+                print("member list: " + f'{indexDict }')
+
+if noformat == True:
+    formatFunction()
+else:
+    noFormattingFunction()
