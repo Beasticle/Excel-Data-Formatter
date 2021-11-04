a74b0339410dc44943ec45a6d871d2645b5e9485
beasticle <julian.heidt@gmail.com>
2021-11-04 19:59:50 -0400

    what the fuck is wrong, it doesn't go thru the functions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from numpy import radians
+from numpy import character
 import pandas as pd
 
 print("\n Please put your excel workbook in the same folder as this script. All spelling needs to be exact.")
@@ -14,9 +14,6 @@ columns = input("\n What columns is your data in? If you have multiple columns, 
 
 filterInput = input("\n What do you want to filter by, eg. scm.uk? ")
 
-workbook = pd.read_excel(f"{workbookName}.xlsx", sheet_name=sheetName)
-workbook.head()
-
 ran = False
 indexDict = defaultdict(list)
 colList = columns.split(", ")
@@ -24,16 +21,27 @@ filterList = filterInput.split(', ')
 stringToParse = ""
 columnList = []
 filterListFull = []
-for data in range(len(filterList)):
-     filterListFull.append(filterList[data].strip(', '))
-for data in range(len(colList)):
-    columnList.append(colList[data].strip(', '))
-
 filterList = filterInput.split(", ")
 
-if filterListFull == []:
-    for index in range(len(workbook[indexCol])):
-        filterListFull.append(workbook[indexCol].iloc[index])
+workbook = pd.read_excel(f"{workbookName}.xlsx", sheet_name=sheetName)
+workbook.head()
+
+print(filterInput)
+
+# for data in range(len(filterList)):
+#     print("stripping filters")
+#     filterListFull.append(filterList[data].strip(', '))
+# for data in range(len(colList)):
+#     print("stripping columns")
+#     columnList.append(colList[data].strip(', '))
+
+# if filterListFull == []:
+#     filterListFull = []
+#     for index in range(len(workbook[indexCol])):
+#         filterListFull.append(workbook[indexCol].iloc[index])
+#         print(filterListFull)
+#         if index >= len(workbook[indexCol]):
+#             break
 
 
 def formatFunction():
@@ -41,19 +49,19 @@ def formatFunction():
 
         if data == workbook[indexCol].iloc[index] and ran == False:
             print(columnList)    
-            print(data, index)
+            print(stringToParse)
             print(filterListFull)
             
             if workbook[stringToParse].iloc[index].split('@')[1] not in indexDict and workbook[stringToParse].iloc[index].split('@')[1] in filterListFull:
                 indexDict[workbook[indexCol].iloc[index]] = [workbook[stringToParse].iloc[index].split('@')[1]]
                 for i in columnList:
-                    #if i not in stringToParse:
+                    print("Formatting, not in dict yet " + workbook[i].iloc[index])
                     indexDict[workbook[indexCol].iloc[index]].extend([workbook[i].iloc[index]])
 
             elif workbook[stringToParse].iloc[index].split('@')[1] in filterListFull:
                 indexDict[workbook[indexCol].iloc[index]].extend([workbook[stringToParse].iloc[index].split('@')[1]])
                 for i in columnList:
-                    #if i not in stringToParse:
+                    print("Formatting, in dict already " + workbook[i].iloc[index])
                     indexDict[workbook[indexCol].iloc[index]].extend([workbook[i].iloc[index]])
             else:
                 print("Value is already in that list or not valid")
@@ -61,9 +69,8 @@ def formatFunction():
             if index == 7229:
                 df = pd.DataFrame.from_dict(indexDict , orient='index')
                 df.to_excel("Test.xlsx", sheet_name="Memberoutput")
-                print("member list: " + f'{indexDict }')
+                print("member list: " + f'{indexDict}')
                 break
-                exit
 
 def noFormattingFunction():
     for index, data in enumerate(workbook[indexCol]):
@@ -76,10 +83,12 @@ def noFormattingFunction():
             if workbook[string0].iloc[index] not in indexDict and workbook[string0].iloc[index] in filterListFull:
                 indexDict[workbook[indexCol].iloc[index]] = []
                 for i in columnList:
+                    print("No formatting, not in dict yet " + workbook[i].iloc[index])
                     indexDict[workbook[indexCol].iloc[index]].extend([workbook[i].iloc[index]])
 
             elif workbook[string0].iloc[index] in filterListFull:
                 for i in columnList:
+                    print("No formatting, in dict already " + workbook[i].iloc[index])
                     indexDict[workbook[indexCol].iloc[index]].extend([workbook[i].iloc[index]])
             else:
                 print("Value is already in that list or not valid")
@@ -89,27 +98,102 @@ def noFormattingFunction():
                 df.to_excel("Test.xlsx", sheet_name="Memberoutput")
                 print("member list: " + f'{indexDict }')
                 break
-                exit
+def noFormattingNoFilter():
+    for index, data in enumerate(workbook[indexCol]):
+
+        if data == workbook[indexCol].iloc[index] and ran == False:
+            print(columnList)    
+            print(data, index)
+            print(filterListFull)
+
+            if workbook[string0].iloc[index] not in indexDict: #and workbook[string0].iloc[index] in filterListFull:
+                indexDict[workbook[indexCol].iloc[index]] = []
+                for i in columnList:
+                    print("No formatting, not in dict yet " + workbook[i].iloc[index])
+                    indexDict[workbook[indexCol].iloc[index]].extend([workbook[i].iloc[index]])
+
+            # elif workbook[string0].iloc[index]: # in filterListFull:
+            #     for i in columnList:
+            #         print("No formatting, in dict already " + workbook[i].iloc[index])
+            #         indexDict[workbook[indexCol].iloc[index]].extend([workbook[i].iloc[index]])
+            else:
+                print("Value is already in that list or not valid")
+
+            if index == 7229:
+                df = pd.DataFrame.from_dict(indexDict , orient='index')
+                df.to_excel("Test.xlsx", sheet_name="Memberoutput")
+                print("member list: " + f'{indexDict }')
+                break
+
+def formatNoFilter():
+    for index, data in enumerate(workbook[indexCol]):
+
+        if data == workbook[indexCol].iloc[index] and ran == False:
+            print(columnList)    
+            print(stringToParse)
+            print(filterListFull)
+            
+            if workbook[stringToParse].iloc[index].split('@')[1]: # not in indexDict and workbook[stringToParse].iloc[index].split('@')[1] in filterListFull:
+                indexDict[workbook[indexCol].iloc[index]] = [workbook[stringToParse].iloc[index].split('@')[1]]
+                for i in columnList:
+                    print("Formatting, not in dict yet " + workbook[i].iloc[index])
+                    indexDict[workbook[indexCol].iloc[index]].extend([workbook[i].iloc[index]])
+
+            # elif workbook[stringToParse].iloc[index].split('@')[1] in filterListFull:
+            #     indexDict[workbook[indexCol].iloc[index]].extend([workbook[stringToParse].iloc[index].split('@')[1]])
+            #     for i in columnList:
+            #         print("Formatting, in dict already " + workbook[i].iloc[index])
+            #         indexDict[workbook[indexCol].iloc[index]].extend([workbook[i].iloc[index]])
+            else:
+                print("Value is already in that list or not valid")
+
+            if index == 7229:
+                df = pd.DataFrame.from_dict(indexDict , orient='index')
+                df.to_excel("Test.xlsx", sheet_name="Memberoutput")
+                print("member list: " + f'{indexDict}')
+                break
 
 
 for col in columnList:
-    if str('@') not in [workbook[col].iloc[0]]:
-        for i in range(len(workbook[col])):
-            print(i)
-            globals()[f"string{i}"] = col
-            noFormat = True
-            ran = True
+    if '@' not in [workbook[col].iloc[0]]:
+        if filterListFull == []:
+            for i in range(len(workbook[col])):
+                #print(i)
+                globals()[f"string{i}"] = col
+                if i >= len(columnList):
+                    noFormattingNoFilter()
+                    ran = True
+                    break
+        else:
+            for i in range(len(workbook[col])):
+                #print(i)
+                globals()[f"string{i}"] = col
+                if i >= len(columnList):
+                    noFormattingFunction()
+                    ran = True
+                    break
     else: 
-        if str('@') in [workbook[col].iloc[0]]:
-            noFormat = False
+        if '@' in [workbook[col].iloc[0]]:
             stringToParse = col
             ran = True
         else:
-            for i in enumerate(workbook[col], start=1):
-                globals()[f"string{i}"] = col
-                ran = True
-
-if noFormat:
-    noFormattingFunction()
-else:
-    formatFunction()
+            if filterListFull == []:
+                for i in enumerate(workbook[col], start=1):
+                    if i == 1:
+                        string0 = col
+                    elif i >= len(columnList):
+                        formatNoFilter()
+                        ran = True
+                        break
+                    else:
+                        globals()[f"string{i}"] = col
+            else:
+                for i in enumerate(workbook[col], start=1):
+                    if i == 1:
+                        string0 = col
+                    elif i >= len(columnList):
+                        formatFunction()
+                        ran = True
+                        break
+                    else:
+                        globals()[f"string{i}"] = col
